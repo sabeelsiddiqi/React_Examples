3780d3a0c8cc3e7ffe73dd5f5dcf2e183de484f0
sabeelsiddiqi <sabeelsiddiqi@gmail.com>
2019-02-08 16:23:20 -0500

    New Examples

---
+++ React_Tutorials/reduxexample/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Provider} from 'react-redux'
+
+
+import Posts from './components/Posts'
+import Postform from './components/Postform'
+
+import store from './store'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Postform/>
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
